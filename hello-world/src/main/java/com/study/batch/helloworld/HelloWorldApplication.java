b2b4043e517c06c0949b20fa3f3cc9316f40dac3
fivestring <fivestring@mz.co.kr>
2023-01-03 15:42:08 +0900

    add: 잡 파라미터 validation 추가

--- hello-world/src/main/java/com/study/batch/helloworld/HelloWorldApplication.java
+++ hello-world/src/main/java/com/study/batch/helloworld/HelloWorldApplication.java
@@ -2,17 +2,21 @@ package com.study.batch.helloworld;
 
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
-import org.springframework.batch.core.StepContribution;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
-import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.configuration.annotation.StepScope;
+import org.springframework.batch.core.job.CompositeJobParametersValidator;
+import org.springframework.batch.core.job.DefaultJobParametersValidator;
 import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+
+import java.util.List;
 
 @EnableBatchProcessing
 @SpringBootApplication
@@ -25,22 +29,67 @@ public class HelloWorldApplication {
     private StepBuilderFactory stepBuilderFactory;
 
     @Bean
+    public CompositeJobParametersValidator validator() {
+        CompositeJobParametersValidator validator = new CompositeJobParametersValidator();
+
+        DefaultJobParametersValidator defaultJobParametersValidator = new DefaultJobParametersValidator(
+                new String[]{"fileName"},
+                new String[]{"name"}
+        );
+
+        defaultJobParametersValidator.afterPropertiesSet();
+
+        validator.setValidators(List.of(new ParameterValidator(), defaultJobParametersValidator));
+
+        return validator;
+    }
+
+    @Bean
     public Step step() {
         return this.stepBuilderFactory.get("step1")
-                .tasklet(new Tasklet() {
-                    @Override
-                    public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
-                        System.out.println("Hello, World!");
-                        return RepeatStatus.FINISHED;
-                    }
-                }).build();
+                .tasklet(((contribution, chunkContext) -> {
+                    System.out.println("Hello, World!");
+                    return RepeatStatus.FINISHED;
+                })).build();
+    }
+
+    @Bean
+    public Step step1() {
+        return this.stepBuilderFactory.get("step2")
+                .tasklet(taskletHelloWorldTasklet(null, null))
+                .build();
     }
 
     @Bean
     public Job job() {
-        return this.jobBuilderFactory.get("job")
-                .start(step())
+        return this.jobBuilderFactory.get("basicJob")
+                .start(step1())
+                .validator(validator())
                 .build();
+    }
+
+    @Bean
+    public Tasklet helloWorldTasklet() {
+        return ((contribution, chunkContext) -> {
+            String name = (String) chunkContext.getStepContext()
+                    .getJobParameters()
+                    .get("name");
+            System.out.printf("Hello, %s!%n", name);
+            return RepeatStatus.FINISHED;
+        });
+    }
+
+    @StepScope
+    @Bean
+    public Tasklet taskletHelloWorldTasklet(
+            @Value("#{jobParameters['name']}") String name,
+            @Value("#{jobParameters['fileName']}") String fileName
+            ) {
+        return ((contribution, chunkContext) -> {
+            System.out.println(String.format("Hello, %s!", name));
+            System.out.println(String.format("fileName = %s!", fileName));
+            return RepeatStatus.FINISHED;
+        });
     }
 
     public static void main(String[] args) {
